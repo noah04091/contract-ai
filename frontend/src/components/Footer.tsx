b6ab76510db3ce79a0ed9f7caf9692d8223fdbb0
noah04091 <liebold.noah@web.de>
2025-04-08 11:23:19 +0200

    fix: rollup native issue + esbuild fix

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,16 +1,15 @@
 // 📁 src/components/Footer.tsx
 import { Link } from "react-router-dom";
-import styles from "../styles/Footer.module.css";
+import styles from "../styles/Footer.module.css"; // Achte darauf, dass diese Datei existiert
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>
-        © {new Date().getFullYear()} Contract AI &nbsp;|&nbsp;
-        <Link to="/impressum">Impressum</Link> &nbsp;|&nbsp;
-        <Link to="/datenschutz">Datenschutz</Link> &nbsp;|&nbsp;
-        <Link to="/agb">AGB</Link>
-      </p>
+      <Link to="/impressum">Impressum</Link>
+      <span>•</span>
+      <Link to="/datenschutz">Datenschutz</Link>
+      <span>•</span>
+      <Link to="/agb">AGB</Link>
     </footer>
   );
 }
