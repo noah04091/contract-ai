9359e8764d39cc7ff36b91373a80aa0834c01744
noah04091 <liebold.noah@web.de>
2025-04-18 13:00:33 +0200

    📝 EditContract-Seite überarbeitet & gestylt

--- frontend/src/pages/EditContract.tsx
+++ frontend/src/pages/EditContract.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-import "./EditContract.css";
+import "./EditContract.module.css";
 
 interface Contract {
   name: string;
